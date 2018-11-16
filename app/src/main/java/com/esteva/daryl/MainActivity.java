e11267ca92d349ff546fc1eb89a20dea5ef84135
Destevaaa <2015081193@ust-ics.mygbiz.com>
2018-11-17 00:02:35 +0800

    Initial commit

---
+++ app/src/main/java/com/esteva/daryl/MainActivity.java
@@ -0,0 +1,13 @@
+package com.esteva.daryl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
